44ce9df6a7e7f17002660b6168afc363f5b7a6a7
r0brcz <rod0brcz@gmail.com>
2023-06-09 15:05:25 -0600

    adding some feedback

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","
 "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+const appContainer = document.querySelector('#container')
 let firstPass = document.querySelector('#pass1');
 let secondPass = document.querySelector('#pass2');
+let generateBtn = document.querySelector('#generate')
+
+generateBtn.addEventListener('click', random);
+
+function random() {
+    firstPass.textContent = generate();
+    secondPass.textContent = generate();
+ 
+ }
 
 function generate(){
     let pass =""
@@ -15,18 +25,33 @@ function generate(){
  return pass
 }
 
-function random() {
-   firstPass.textContent = generate();
-   secondPass.textContent = generate();
+//function copy() {
+  //  navigator.clipboard.writeText(firstPass.textContent);
+//}
 
-}
+firstPass.addEventListener('click', function(){
+    copyPasswordToClipboard(firstPass);
+});
 
-function copy() {
-    navigator.clipboard.writeText(firstPass.textContent);
-    var tooltip = document.querySelector('#myTooltip');
-    tooltip.textContent = 'Copied'
-}
+secondPass.addEventListener('click', function(){
+    copyPasswordToClipboard(secondPass);
+})
 
-function copy2() {
-    navigator.clipboard.writeText(secondPass.textContent);
+function copyPasswordToClipboard(passwordElement) {
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(passwordElement.textContent);
+      // Display feedback message to user
+      const feedbackDiv = document.createElement('p');
+      feedbackDiv.textContent = 'Copied';
+      feedbackDiv.style.color = '#D5D4D8';
+      feedbackDiv.style.textAlign = 'center'
+      appContainer.insertBefore(feedbackDiv, appContainer.firstChild);
+      // Hide feedback message after 2 seconds
+      setTimeout(() => {
+        appContainer.removeChild(feedbackDiv);
+      }, 500);
+    } else {
+      // Clipboard API not supported
+      console.log('Clipboard API not supported');
+    }
 }
